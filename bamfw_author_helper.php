919f3a0587f89fc06db31c4387ddabb53ffe215f
Sebastian Weiss <54996215+3ele-projects@users.noreply.github.com>
2022-08-24 09:49:17 +0200

    Update bamfw_author_helper.php

--- bamfw_author_helper.php
+++ bamfw_author_helper.php
@@ -14,13 +14,16 @@
 
 // Your code starts here.
 
-add_action( 'acf/save_post', 'set_user_by_author_id', 10, 2 ); 
+
 define('BAH_PATH', plugin_dir_path(__FILE__));
 
 include( plugin_dir_path( __FILE__ ) . 'acf/acf.php');
 
+
+add_action( 'acf/save_post', 'set_user_by_author_id', 10, 2 ); 
+
 function set_user_by_author_id($post_id){
-    $lwm_author = get_field('lwm_author', $post_id);
+    $lwm_author = get_field('author', $post_id);
     if ($lwm_author){
         $arg = array(
             'ID' => $post_id,
